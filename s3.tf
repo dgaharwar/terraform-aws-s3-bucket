a146a2f23c942f4d1da0dcf2cffa4a48d1a67ce6
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-03-05 13:57:10 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 0.12.20"
+
+  required_providers {
+    aws = ">= 2.51"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket
   force_destroy = var.force_destroy
